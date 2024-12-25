78de44c7ca8d7c1b011e02c8326af51a492dae9c
Faatah <guure781@gmail.com>
2024-12-25 15:41:20 +0300

    done exercise for calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,23 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculator(num1,num2,operator){
+
+    if(operator=="+"){
+        console.log(num1+num2)
+    }else if(operator=="-"){
+        console.log(num1-num2)
+    }else if(operator=="/"){
+        console.log(num1 / num2)
+    }else if(operator=="*"){
+        console.log(num1*num2)
+    }
+    else if (operator!="+","-","/","*"){
+        console.log("invalid operator")
+    }
+    
+}
+ calculator(2,3,"+")
+
+
+
